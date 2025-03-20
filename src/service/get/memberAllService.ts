cf38fb2c6c00ca23765770f8d8c54d6d6959b172
Rohyj <yacheku@naver.com>
2025-03-21 02:49:47 +0900

    azure update

--- src/service/get/memberAllService.ts
+++ src/service/get/memberAllService.ts
@@ -5,22 +5,20 @@ import { responseError } from "../../util/error/service/responseError";
 
 // 로그인 한 유저의 데이터를 서버 요청함 (email = 로그인 한 유저)
 export const fetchMember = async (
-  email: string | null
+  email: string
 ): Promise<MemberResponseType> => {
   const token = localStorage.getItem("token");
 
   try {
-    const response = await axios
-      .get<MemberResponseType>(
-        `https://ott-roh-frangnc6aaa2dycw.koreacentral-01.azurewebsites.net/member/member?email=${email}`,
-        {
-          headers: {
-            Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 포함
-          },
-        }
-      )
-      .then((res) => res.data);
-    return response;
+    const response = await axios.get<MemberResponseType>(
+      `https://ott-roh-frangnc6aaa2dycw.koreacentral-01.azurewebsites.net/member/member?email=${email}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`, // JWT 토큰을 헤더에 포함
+        },
+      }
+    );
+    return response.data;
   } catch (error) {
     if (axios.isAxiosError(error)) {
       error.response
@@ -30,6 +28,6 @@ export const fetchMember = async (
       // 그 외의 에러
       console.error("알 수 없는 에러가 발생했습니다:", error);
     }
-    throw error; // 에러를 다시 던져서 호출하는 곳에서 처리할 수 있게 합니다.
+    throw error; // 에러를 다시 던져서 호출할 수 있게 합니다.
   }
 };
